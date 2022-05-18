f3722c96ebc32d711e7ce9a61d3caad12a26c15d
Christopher Harris <harriscm@pm.me>
2022-05-18 02:03:43 -0400

    refactor: Made Repacker DRY via RepackerBase superclass

--- compass/repacker/repacker_xml.py
+++ compass/repacker/repacker_xml.py
@@ -1,21 +1,13 @@
 import logging
-import os
-from pathlib import Path
-import re
-from typing import List
 
 from compass import regex as rx
-from compass.datatype import Cipher, TranslateType
+from compass.datatype import TranslateType
+from compass.repacker.repacker_base import BaseRepacker
 
 LOGGER = logging.getLogger(__name__)
 
 
-class XMLRepacker:
-    def __init__(self, filenames: List[Path], cipher: Cipher, output_root: str):
-        self.filenames_base = filenames
-        self.cipher = cipher
-        self.output_root = output_root
-
+class XMLRepacker(BaseRepacker):
     def repack(self):
         LOGGER.info("Starting...")
 
@@ -46,34 +38,3 @@ class XMLRepacker:
 
             LOGGER.info(f"|{filename}| Saving...")
             self.write_contents(filename, contents)
-
-    def write_contents(self, filename: str, contents: List[str]):
-        # Prepend Output Root to Filename
-        write_path = Path(self.output_root + os.path.sep + filename)
-
-        # Generate all subdirectories from output root
-        write_path.parents[0].mkdir(parents=True, exist_ok=True)
-
-        with open(write_path, "w+", encoding="windows-1251") as fp:
-            fp.writelines(contents)
-
-    def get_base_contents(self, filename: str):
-        filename_base = None
-        for filename_candidate in self.filenames_base:
-            if str(filename_candidate.resolve()).endswith(filename):
-                filename_base = filename_candidate
-                break
-
-        if not filename_base:
-            raise Exception(f"File Not Found: {filename}")
-
-        with open(filename_base, "r", encoding="windows-1251") as fp:
-            contents_base = fp.readlines()
-
-        return contents_base
-
-    @staticmethod
-    def _text_replace(regex: re.Pattern, text_replace: str, text_old: str):
-        r = regex.search(text_old).span(1)
-        text_new = text_old[:r[0]] + text_replace + text_old[r[1]:]
-        return text_new
